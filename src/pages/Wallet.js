79444e1b399797c88a7fdf4c00dd8cd5d8a21a12
Kai <human2lkai@gmail.com>
2022-04-19 11:29:33 +1000

    Wallet: add sign method, text and sound effects

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,14 @@
 import { useState, useEffect } from "react";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import useSound from "use-sound";
 import { styled } from "@mui/material/styles";
 import walletSVG from "../assets/wallet.svg";
-import { getDanMoney } from "../utils/airtable";
+import dollarPng from "../assets/images/dollar.png";
+import moneySound from "../assets/sounds/multipleCoins.mp3";
+import ahOhSound from "../assets/sounds/ah-oh.mp3";
+import { getWalletState, updateDanWalletState } from "../utils/airtable";
 
 const WalletContainer = styled("div")({
   paddingTop: "100px",
@@ -21,32 +27,115 @@ const ItemContainer = styled("div")({
   alignItems: "center",
 });
 
+const canSignIn = (date1, date2) => {
+  return date2 - date1 > 86400000; // 1 day
+};
+
 export const Wallet = () => {
+  const [danWalletId, setDanWalletId] = useState(undefined);
   const [danMoney, setDanMoney] = useState("");
+  const [lastSignInDate, setLastSignInDate] = useState(Infinity);
+  const [warningMessages, setWarningMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [playMoneySound] = useSound(moneySound);
+  const [playAhOhSound] = useSound(ahOhSound);
+
+  const handleSignIn = async () => {
+    setIsLoading(true);
+    const now = new Date().getTime();
+    const newDanMoney = Number(danMoney) + 0.1;
+    await updateDanWalletState(danWalletId, newDanMoney, now, onUpdateFinish);
+    playMoneySound();
+  };
+
+  const onUpdateFinish = async () => {
+    console.log("updatefinish");
+    await refreshStates();
+  };
+
+  const nextSignInAllowedTime = () => {
+    const nextTime =
+      (lastSignInDate + 86400000 - new Date().getTime()) / (1000 * 60 * 60);
+    if (nextTime < 0) return 0;
+    return nextTime.toFixed(2);
+  };
+
+  const refreshStates = async () => {
+    console.log("refreshing");
+    const walletState = await getWalletState();
+    setDanWalletId(walletState[1].id);
+    setDanMoney(walletState[1].fields.number.toFixed(2));
+    setLastSignInDate(walletState[1].fields.lastSignInDate);
+    setIsLoading(false);
+  };
 
   useEffect(() => {
     (async () => {
-      setDanMoney(await getDanMoney());
+      await refreshStates();
     })();
   }, []);
 
   return (
-    <WalletContainer>
-      <img src={walletSVG} alt="wallet" height="50px" width="50px" />
-      <ItemContainer>
-        <Typography color="initial" variant="h5">
-          蛋蛋账户余额
-        </Typography>
-        <Typography color="textPrimary" variant="h5">
-          $ {danMoney}
-        </Typography>
-        <Typography color="initial" variant="h5">
-          凯凯账户余额
-        </Typography>
-        <Typography color="secondary" variant="h5">
-          灰常多，老有钱了
-        </Typography>
-      </ItemContainer>
-    </WalletContainer>
+    <>
+      <WalletContainer>
+        <img src={walletSVG} alt="wallet" height="50px" width="50px" />
+        <ItemContainer>
+          <Typography color="initial" variant="h5">
+            蛋蛋账户余额
+          </Typography>
+          <Typography color="textPrimary" variant="h5">
+            $ {danMoney}
+          </Typography>
+          {/* <Typography color="initial" variant="h5">
+            凯凯账户余额
+          </Typography>
+          <Typography color="secondary" variant="h5">
+            灰常多，老有钱了
+          </Typography> */}
+          {canSignIn(lastSignInDate, new Date().getTime()) ? (
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<Avatar src={dollarPng} />}
+              onClick={handleSignIn}
+            >
+              <Typography sx={{ color: "white" }} variant="h5">
+                {isLoading ? "正在签到" : "签到"}
+              </Typography>
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<Avatar src={dollarPng} />}
+              onClick={() => {
+                setWarningMessages([
+                  ...warningMessages,
+                  "别点啦，已经给过钱钱啦！",
+                ]);
+                playAhOhSound();
+              }}
+            >
+              <Typography sx={{ color: "white" }} variant="h5">
+                已签到
+              </Typography>
+            </Button>
+          )}
+          <Typography color="secondary" variant="h6">
+            距离下次签到时间：{nextSignInAllowedTime()}小时
+          </Typography>
+          {warningMessages.map((warningMessage, index) => {
+            return (
+              <Typography key={index} color="primary" variant="h5">
+                {warningMessage}
+              </Typography>
+            );
+          })}
+        </ItemContainer>
+      </WalletContainer>
+      <a href="https://www.flaticon.com/free-icons/coin" title="coin icons">
+        Coin icons created by Freepik - Flaticon
+      </a>
+    </>
   );
 };
