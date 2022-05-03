d2649e2f4b20f09c5dcb8d8ad2aa8722a57f5089
Kai <human2lkai@gmail.com>
2022-05-03 11:34:24 +1000

    Update Diarys.js
    border-box sizing

--- src/pages/Diarys.js
+++ src/pages/Diarys.js
@@ -13,9 +13,11 @@ import { getAllDiarys } from "../services/airtable";
 import { getCountryDateFromTimestamp } from "../utils/date_utils";
 
 const LoveDiaryContainer = styled("div")({
+  boxSizing: "border-box",
   marginLeft: 10,
   marginRight: 10,
   paddingBottom: 65,
+  // maxWidth: 500,
   display: "flex",
   flexDirection: "column",
 });
