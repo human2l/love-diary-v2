33cb59ecba927df8d688ead448bf9a3417b9deb8
Kai <human2lkai@gmail.com>
2022-05-03 17:09:44 +1000

    Delete and move back all files to make sure Netlify works

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import musicReducer from "../features/music/musicSlice";
-
-export default configureStore({
-  reducer: {
-    music: musicReducer,
-  },
-});
