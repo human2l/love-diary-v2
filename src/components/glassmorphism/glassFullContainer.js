821afd390eb9062319585db8048524de794e3aad
Kai <human2lkai@gmail.com>
2022-05-07 13:33:08 +1000

    Create glassFullContainer.js

---
+++ src/components/glassmorphism/glassFullContainer.js
@@ -0,0 +1,19 @@
+const GlassFullContainer = (props) => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        background: `rgba(255,255,255,0.3)`,
+        // background: `rgba(0,0,0,0.5)`,
+        backdropFilter: "blur(1px)",
+        boxShadow: "10px 10px 10px rgba(30,30,30,0.5)",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+export default GlassFullContainer;
