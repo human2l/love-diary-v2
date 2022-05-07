781524a526f0118b9eeacd79db85024012c0ec2b
Kai <human2lkai@gmail.com>
2022-05-07 13:33:12 +1000

    Create glassRoundContainer.js

---
+++ src/components/glassmorphism/glassRoundContainer.js
@@ -0,0 +1,15 @@
+const GlassRoundContainer = (props) => {
+  return (
+    <div
+      style={{
+        borderRadius: 20,
+        background: `rgba(255,255,255,0.7)`,
+        backdropFilter: "blur(1px)",
+        boxShadow: "10px 10px 10px rgba(30,30,30,0.5)",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+export default GlassRoundContainer;
