e52c785c8a08dbe918c34dc49867058e105e2b32
Kai <human2lkai@gmail.com>
2022-05-10 21:54:16 +1000

    Update todoListHistory.js

--- src/pages/todoListHistory.js
+++ src/pages/todoListHistory.js
@@ -11,13 +11,13 @@ import PageLoading from "../components/pageLoading";
 import { getAllTodosHistory } from "../services/airtable";
 import { getCountryDateFromTimestamp } from "../utils/date_utils";
 
-const TodoListHistoryContainer = styled("div")({
-  boxSizing: "border-box",
+const TodoListHistoryCardsContainer = styled("div")({
+  paddingTop: "70px",
   marginLeft: 10,
   marginRight: 10,
-  paddingBottom: 65,
   display: "flex",
   flexDirection: "column",
+  paddingBottom: 65,
 });
 
 const CardContainer = styled(Card)({
@@ -37,6 +37,32 @@ const TodoContainer = styled("div")({
   display: "flex",
 });
 
+const TodoListDashBoard = styled("div")({
+  position: "fixed",
+  height: "70px",
+  width: "100%",
+  background: `rgba(255,255,255,0.8)`,
+  borderBottomLeftRadius: 10,
+  borderBottomRightRadius: 10,
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "center",
+});
+
+const AchievedGroup = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const FinishedRateGroup = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
 const TodoListHistory = () => {
   const { settings } = useContext(settingsContext);
 
@@ -50,48 +76,72 @@ const TodoListHistory = () => {
       {isLoading ? (
         <PageLoading />
       ) : (
-        <TodoListHistoryContainer>
-          {todosHistory.map((history) => {
-            return (
-              <CardContainer key={history.id}>
-                <Card>
-                  <CardContent>
-                    <TitleContainer>
-                      <Typography
-                        variant="h5"
-                        color={settings[history.user].primaryColor}
-                        gutterBottom
-                      >
-                        {history.user}
-                      </Typography>
-                      <TodosMetaContainer>
-                        <Typography color="textSecondary">
-                          {getCountryDateFromTimestamp(
-                            history.time,
-                            settings[history.user].country
-                          )}
-                        </Typography>
-                      </TodosMetaContainer>
-                    </TitleContainer>
+        <>
+          <TodoListDashBoard>
+            <FinishedRateGroup>
+              <Typography variant="h5">Finished Rate</Typography>
+              <Typography>100%</Typography>
+            </FinishedRateGroup>
+            <AchievedGroup>
+              <Typography variant="h5">Achieved</Typography>
+              <Typography>123</Typography>
+            </AchievedGroup>
+            <Typography variant="h2">Dan</Typography>
+            <Typography variant="h2">vs</Typography>
+            <Typography variant="h2">Kai</Typography>
+            <AchievedGroup>
+              <Typography variant="h5">Achieved</Typography>
+              <Typography>123</Typography>
+            </AchievedGroup>
+            <FinishedRateGroup>
+              <Typography variant="h5">Finished Rate</Typography>
+              <Typography>100%</Typography>
+            </FinishedRateGroup>
+          </TodoListDashBoard>
 
-                    {history.todos.map((todo) => {
-                      return (
-                        <TodoContainer key={todo.id}>
-                          {todo.done ? (
-                            <CheckCircleIcon color="success" />
-                          ) : (
-                            <WarningIcon color="error" />
-                          )}
-                          <Typography sx={{ ml: 1 }}>{todo.name}</Typography>
-                        </TodoContainer>
-                      );
-                    })}
-                  </CardContent>
-                </Card>
-              </CardContainer>
-            );
-          })}
-        </TodoListHistoryContainer>
+          <TodoListHistoryCardsContainer>
+            {todosHistory.map((history) => {
+              return (
+                <CardContainer key={history.id}>
+                  <Card>
+                    <CardContent>
+                      <TitleContainer>
+                        <Typography
+                          variant="h5"
+                          color={settings[history.user].primaryColor}
+                          gutterBottom
+                        >
+                          {history.user}
+                        </Typography>
+                        <TodosMetaContainer>
+                          <Typography color="textSecondary">
+                            {getCountryDateFromTimestamp(
+                              history.time,
+                              settings[history.user].country
+                            )}
+                          </Typography>
+                        </TodosMetaContainer>
+                      </TitleContainer>
+
+                      {history.todos.map((todo) => {
+                        return (
+                          <TodoContainer key={todo.id}>
+                            {todo.done ? (
+                              <CheckCircleIcon color="success" />
+                            ) : (
+                              <WarningIcon color="error" />
+                            )}
+                            <Typography sx={{ ml: 1 }}>{todo.name}</Typography>
+                          </TodoContainer>
+                        );
+                      })}
+                    </CardContent>
+                  </Card>
+                </CardContainer>
+              );
+            })}
+          </TodoListHistoryCardsContainer>
+        </>
       )}
     </>
   );
