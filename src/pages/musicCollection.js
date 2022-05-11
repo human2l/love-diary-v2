abf72aae29dcb5026685d2bcb1fd4f7b404b3bf1
Kai <human2lkai@gmail.com>
2022-05-11 18:41:55 +1000

    Update musicCollection.js
    Update style

--- src/pages/musicCollection.js
+++ src/pages/musicCollection.js
@@ -7,6 +7,7 @@ import ListItemText from "@mui/material/ListItemText";
 import { styled } from "@mui/material/styles";
 import { useContext, useState } from "react";
 import { settingsContext } from "../app";
+import GlassRoundContainer from "../components/glassmorphism/glassRoundContainer";
 import useSoundLibrary from "../hooks/useSoundLibrary";
 
 const MusicCollectionContainer = styled("div")({
@@ -14,9 +15,22 @@ const MusicCollectionContainer = styled("div")({
   marginLeft: "auto",
   marginRight: "auto",
   paddingBottom: 65,
+});
+
+const ContentContainer = styled("div")({
+  marginTop: 50,
+  paddingBottom: 50,
   display: "flex",
+  marginLeft: "auto",
+  marginRight: "auto",
   flexDirection: "column",
   alignItems: "center",
+  width: "500px",
+});
+
+const ListContainer = styled("div")({
+  width: "100%",
+  backgroundColor: "white",
 });
 
 const MusicCollection = () => {
@@ -32,26 +46,32 @@ const MusicCollection = () => {
   };
   return (
     <MusicCollectionContainer>
-      <Typography variant="h3" color="primary">
-        Music List
-      </Typography>
-      <Box sx={{ minWidth: "50%", bgcolor: "background.paper" }}>
-        <List component="nav" dense>
-          {musicList.map((music, index) => {
-            return (
-              <div key={index}>
-                <ListItemButton
-                  selected={selectedIndex === index}
-                  onClick={(event) => handleListItemClick(event, index)}
-                >
-                  <ListItemText primary={music.name} />
-                </ListItemButton>
-                <Divider />
-              </div>
-            );
-          })}
-        </List>
-      </Box>
+      <GlassRoundContainer>
+        <ContentContainer>
+          <Typography variant="h3" color="primary">
+            Music List
+          </Typography>
+          <ListContainer>
+            <Box sx={{ minWidth: "50%", bgcolor: "background.paper" }}>
+              <List component="nav" dense>
+                {musicList.map((music, index) => {
+                  return (
+                    <div key={index}>
+                      <ListItemButton
+                        selected={selectedIndex === index}
+                        onClick={(event) => handleListItemClick(event, index)}
+                      >
+                        <ListItemText primary={music.name} />
+                      </ListItemButton>
+                      <Divider />
+                    </div>
+                  );
+                })}
+              </List>
+            </Box>
+          </ListContainer>
+        </ContentContainer>
+      </GlassRoundContainer>
     </MusicCollectionContainer>
   );
 };
