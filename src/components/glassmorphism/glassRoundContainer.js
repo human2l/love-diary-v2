308333225cd303a8c26b7e0fde3973813196397e
Kai <human2lkai@gmail.com>
2022-05-28 16:50:52 +1000

    Update glassRoundContainer.js
    Add animation

--- src/components/glassmorphism/glassRoundContainer.js
+++ src/components/glassmorphism/glassRoundContainer.js
@@ -1,15 +1,20 @@
+import Zoom from "@mui/material/Zoom";
+
 const GlassRoundContainer = (props) => {
   return (
-    <div
-      style={{
-        borderRadius: 20,
-        background: `rgba(255,255,255,0.8)`,
-        backdropFilter: "blur(1px)",
-        boxShadow: "10px 10px 10px rgba(30,30,30,0.5)",
-      }}
-    >
-      {props.children}
-    </div>
+    <Zoom in timeout={500}>
+      <div
+        style={{
+          borderRadius: 20,
+          background: `rgba(255,255,255,0.8)`,
+          backdropFilter: "blur(1px)",
+          boxShadow: "10px 10px 10px rgba(30,30,30,0.5)",
+        }}
+      >
+        {props.children}
+      </div>
+    </Zoom>
   );
 };
+
 export default GlassRoundContainer;
