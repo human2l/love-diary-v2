42e561c2f211fe7c42a684a6345b2404e2c2f7c5
Kai <human2lkai@gmail.com>
2022-06-03 00:51:15 +1000

    Update style of login page to match big screen

--- src/pages/login.js
+++ src/pages/login.js
@@ -8,15 +8,21 @@ import { settingsContext } from "../app";
 import popDownSound from "../assets/sounds/pop-down.mp3";
 import popUpOffSound from "../assets/sounds/pop-up-off.mp3";
 import popUpOnSound from "../assets/sounds/pop-up-on.mp3";
+import GlassRoundContainer from "../components/glassmorphism/glassRoundContainer";
 
 const LoginLabel = styled(Typography)`
-  border-radius: 50px;
+  border-radius: 20px;
   text-align: center;
 `;
 
 const LoginContainer = styled.div`
+  margin-top: 20%;
+  height: 100%;
+  width: 95%;
+  margin-left: auto;
+  margin-right: auto;
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
 `;
 
@@ -78,73 +84,76 @@ const Login = (props) => {
 
   return (
     <LoginContainer>
-      <LoginControlContainer>
-        <LoginLabel
-          sx={{ color: "primary.main", backgroundColor: "text.light" }}
-          variant="h4"
-        >
-          {t("password_required.label")}🤪
-        </LoginLabel>
-        <PasswordPad>
-          <Grid
-            container
-            spacing={1}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-            }}
+      <GlassRoundContainer>
+        <LoginControlContainer>
+          <LoginLabel
+            sx={{ color: "primary.main", backgroundColor: "text.light" }}
+            variant="h4"
           >
-            {padArray.map((row, arrayIndex) => {
-              return (
-                <Grid container item xs={12} spacing={1} key={arrayIndex}>
-                  {row.map((number, numberIndex) => {
-                    return (
-                      <Grid item xs={4} key={numberIndex}>
-                        <Button
-                          variant="contained"
-                          color="secondary"
-                          onClick={() => {
-                            onPasswordButtonClick(number);
-                          }}
-                          style={{
-                            borderRadius: "20px",
-                            width: "100%",
-                            maxWidth: "130px",
-                            aspectRatio: "1/1",
-                          }}
-                          onMouseDown={playActive}
-                          onMouseUp={() => {
-                            playOff();
-                          }}
-                        >
-                          <PasswordButtonText
-                            sx={{ color: "white" }}
-                            variant="h1"
+            {t("password_required.label")}🤪
+          </LoginLabel>
+          <PasswordPad>
+            <Grid
+              container
+              spacing={1}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              {padArray.map((row, arrayIndex) => {
+                return (
+                  <Grid container item xs={12} spacing={1} key={arrayIndex}>
+                    {row.map((number, numberIndex) => {
+                      return (
+                        <Grid item xs={4} key={numberIndex}>
+                          <Button
+                            variant="contained"
+                            color="secondary"
+                            onClick={() => {
+                              onPasswordButtonClick(number);
+                            }}
+                            style={{
+                              borderRadius: "20px",
+                              width: "100%",
+                              maxWidth: "130px",
+                              aspectRatio: "1/1",
+                            }}
+                            onMouseDown={playActive}
+                            onMouseUp={() => {
+                              playOff();
+                            }}
                           >
-                            {number}
-                          </PasswordButtonText>
-                        </Button>
-                      </Grid>
-                    );
-                  })}
-                </Grid>
-              );
-            })}
-          </Grid>
-        </PasswordPad>
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          onClick={login}
-          onMouseDown={playActive}
-          onMouseUp={() => {
-            playOn();
-          }}
-        >
-          {loggingIn ? "Logging In..." : t("login.label")}
-        </Button>
-      </LoginControlContainer>
+                            <PasswordButtonText
+                              sx={{ color: "white" }}
+                              variant="h1"
+                            >
+                              {number}
+                            </PasswordButtonText>
+                          </Button>
+                        </Grid>
+                      );
+                    })}
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </PasswordPad>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={login}
+            onMouseDown={playActive}
+            onMouseUp={() => {
+              playOn();
+            }}
+            sx={{ mb: "10px", borderRadius: "20px" }}
+          >
+            {loggingIn ? "Logging In..." : t("login.label")}
+          </Button>
+        </LoginControlContainer>
+      </GlassRoundContainer>
     </LoginContainer>
   );
 };
