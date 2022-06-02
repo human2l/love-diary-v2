7f5103fb9692ec0e59d7ac7bbe3cd4196f7c90b0
Kai <human2lkai@gmail.com>
2022-06-02 18:37:25 +1000

    Update settingsService.js

--- src/services/airtable/settingsService.js
+++ src/services/airtable/settingsService.js
@@ -2,13 +2,26 @@ import base from "./airtable";
 
 const settingsBase = base("settings");
 
-const getUserSettings = async () => {
-  const response = await settingsBase.select({}).all();
-  const userSettings = {};
-  for (let record of response) {
-    userSettings[record.fields.name] = { ...record.fields, id: record.id };
-  }
-  return userSettings;
+const getSettingsByUserId = async (userId) => {
+  const record = await settingsBase.find(userId);
+  return { ...record.fields, id: record.id };
+  // const response = await settingsBase.select({}).all();
+  // const userSettings = {};
+  // for (let record of response) {
+  //   userSettings[record.fields.name] = { ...record.fields, id: record.id };
+  // }
+  // return userSettings;
+};
+
+const getCoupleSettingsByUserId = async (userId) => {
+  const userSettings = await getSettingsByUserId(userId);
+  const partnerSettings = await getSettingsByUserId(userSettings.partnerId);
+  const settings = {
+    [userSettings.name]: { ...userSettings },
+    [partnerSettings.name]: { ...partnerSettings },
+  };
+  console.log(settings);
+  return settings;
 };
 
 const updateSettingsDB = async (settings) => {
@@ -29,4 +42,4 @@ const updateSettingsDB = async (settings) => {
   });
 };
 
-export { getUserSettings, updateSettingsDB };
+export { getCoupleSettingsByUserId, updateSettingsDB };
